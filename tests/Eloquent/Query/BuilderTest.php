bf7703cedc58f0013072aed5938bb3c54de151f1
Dmitriy Tyurin <fobia3d@gmail.com>
2016-10-08 18:51:51 +0300

    add tests Builder

--- tests/Eloquent/Query/BuilderTest.php
+++ tests/Eloquent/Query/BuilderTest.php
@@ -3,6 +3,7 @@ namespace Fobia\Database\SphinxConnection\Test\Eloquent\Query;
 
 use Fobia\Database\SphinxConnection\Eloquent\Query\Builder;
 use Fobia\Database\SphinxConnection\Test\TestCase;
+use Foolz\SphinxQL\Match;
 
 class BuilderTest extends TestCase
 {
@@ -43,12 +44,42 @@ class BuilderTest extends TestCase
 
     /**
      * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::toSql
-     * @todo   Implement testToSql().
      */
     public function testToSql()
     {
         $s = $this->q->select()->toSql();
         $this->assertQuery('select * FROM rt', $s);
+    }
+
+    public function test_intType()
+    {
+        $q = $this->q->where('id', 1);
+        $this->assertQuery("select * FROM rt where id = 1", $q);
+    }
+
+    public function test_stringType()
+    {
+        $q = $this->q->where('id', '1');
+        $this->assertQuery("select * FROM rt where id = '1'", $q);
+    }
+
+    public function test_floatType()
+    {
+        $q = $this->q->where('id', 1.1);
+        $this->assertQuery("select * FROM rt where id = 1.100000", $q);
+    }
+
+    public function test_mvaType()
+    {
+        $q = $this->q->where('id', [1, 2, 3]);
+        $this->assertQuery("select * FROM rt where id = (1, 2, 3)", $q);
+    }
+
+
+    public function test_boolType()
+    {
+        $q = $this->q->where('id', true);
+        $this->assertQuery("select * FROM rt where id = 1", $q);
     }
 
     /**
@@ -62,12 +93,22 @@ class BuilderTest extends TestCase
             'tags' => [1, 2, 3],
             'gid' => 1,
             'greal' => 1.5,
-            'gbool' => true
+            'gbool' => true,
         ]);
 
         $this->assertEquals(1, $r);
         $this->assertQuery("insert into rt (id, name, tags, gid, greal, gbool) values (1, 'name', (1, 2, 3), 1, 1.5, 1))");
     }
+
+    public function testSelect()
+    {
+        $q = $this->makeQ()->select('id');
+        $this->assertQuery("select id FROM rt", $q);
+
+        $q = $this->makeQ()->select('id', 'name');
+        $this->assertQuery("select id, name FROM rt", $q);
+    }
+
     /**
      * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::replace
      */
@@ -79,7 +120,7 @@ class BuilderTest extends TestCase
             'tags' => [4, 5, 6],
             'gid' => 2,
             'greal' => 2.5,
-            'gbool' => true
+            'gbool' => true,
         ]);
 
         $this->assertEquals(1, $r);
@@ -99,7 +140,7 @@ class BuilderTest extends TestCase
             'gid' => 7,
             'greal' => 8.6,
             'tags' => [1, 2, 3, 4, 5],
-            'gbool' => true
+            'gbool' => true,
         ]);
         $this->assertEquals(1, $r);
     }
@@ -111,45 +152,89 @@ class BuilderTest extends TestCase
     public function testWhereMulti()
     {
         // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     /**
-     * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::options
-     * @todo   Implement testOptions().
+     * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::option
      */
-    public function testOptions()
+    public function testOption()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $q = $this->q->option('ranker', 'bm25');
+        $this->assertQuery('select * from rt OPTION ranker = bm25', $q);
+
+        $q->option('max_matches', '3000');
+        $this->assertQuery('select * from rt OPTION ranker = bm25,max_matches=3000', $q);
+
+        $q->option('field_weights', '(title=10, body=3)');
+        $this->assertQuery('select * from rt OPTION ranker = bm25,max_matches=3000, field_weights=(title=10, body=3)',
+            $q);
+
+        $q->option('agent_query_timeout', '10000');
+        $this->assertQuery('select * from rt OPTION ranker = bm25,max_matches=3000, field_weights=(title=10, body=3) , agent_query_timeout=10000',
+            $q);
+        $q->get();
+    }
+
+    /**
+     * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::option
+     */
+    public function testOption2()
+    {
+        $q = $this->q->option('field_weights', ['title' => 10, 'body' => 3]);
+        $this->assertQuery('select * from rt OPTION field_weights=(title=10, body=3)', $q);
+
+        $q->option('comment', 'my comment');
+        $this->assertQuery('select * from rt OPTION field_weights=(title=10, body=3), comment=\'my comment\'', $q);
     }
 
     /**
      * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::withinGroupOrderBy
-     * @todo   Implement testWithinGroupOrderBy().
      */
     public function testWithinGroupOrderBy()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $q = $this->q->select('id');
+        $q = $q->withinGroupOrderBy('name');
+        $this->assertQuery('SELECT id FROM rt WITHIN GROUP ORDER BY name ASC', $q);
+
+        $q = $q->withinGroupOrderBy('id', 'desc');
+        $this->assertQuery('SELECT id FROM rt WITHIN GROUP ORDER BY name ASC, id DESC', $q);
+    }
+
+    /**
+     * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::withinGroupOrderBy
+     * @expectedException \RuntimeException
+     */
+    public function testWithinGroupOrderByException()
+    {
+        $q = $this->q->select('id');
+        $q = $q->withinGroupOrderBy('name', 'a');
     }
 
     /**
      * @covers \Fobia\Database\SphinxConnection\Eloquent\Query\Builder::match
-     * @todo   Implement testMatch().
      */
     public function testMatch()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $q = $this->q->match('text match');
+        $this->assertQuery("select * FROM rt WHERE MATCH('(@text match)')", $q);
+
+        $q = $this->makeQ()->match(['name'], 'match');
+        $this->assertQuery("select * FROM rt WHERE MATCH('(@(name) match)')", $q);
+
+        $q = $this->makeQ()->match(['name', 'content'], 'match');
+        $this->assertQuery("select * FROM rt WHERE MATCH('(@(name,content) match)')", $q);
+
+        $q = $this->makeQ()->match(function(Match $m) {
+            $m->match('match');
+        });
+        $this->assertQuery("select * FROM rt WHERE MATCH('(match)')", $q);
+
+        $q = $this->makeQ()->match(function(Match $m) {
+            $m->field('name');
+            $m->match('match');
+        });
+        $this->assertQuery("select * FROM rt WHERE MATCH('(@name match)')", $q);
     }
 
     /**
@@ -159,9 +244,7 @@ class BuilderTest extends TestCase
     public function testFacet()
     {
         // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     /**
@@ -171,9 +254,7 @@ class BuilderTest extends TestCase
     public function testFilterParamsUint()
     {
         // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
 }
