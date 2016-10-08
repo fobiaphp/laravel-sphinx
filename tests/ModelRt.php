08c066e3423151dfff94d27b37c06508b896cbbe
Dmitriy Tyurin <fobia3d@gmail.com>
2016-10-08 17:37:39 +0300

    add tests

---
+++ tests/ModelRt.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Model.php file
+ *
+ * @author     Dmitriy Tyurin <fobia3d@gmail.com>
+ * @copyright  Copyright (c) 2016 Dmitriy Tyurin
+ */
+
+namespace Fobia\Database\SphinxConnection\Test;
+
+use Fobia\Database\SphinxConnection\Eloquent\Model as BaseModel;
+
+
+/**
+ * Class Model
+ *
+ * @property integer $id                Institution address_id
+ * @property string $name               Institution name
+ *
+ * @author     Dmitriy Tyurin <fobia3d@gmail.com>
+ * @copyright  Copyright (c) 2016 Dmitriy Tyurin
+ */
+class ModelRt extends BaseModel
+{
+    protected $table = 'rt';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'content',
+        'gid',
+        'strname',
+        'tags',
+        'factors'
+    ];
+
+    protected $guarded = ['*'];
+
+    protected $appends = [ ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'tags' => 'mva',
+        'factors' => 'json',
+    ];
+}
